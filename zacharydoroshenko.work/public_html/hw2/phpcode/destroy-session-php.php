05932d02dbb6b2dbd902e910c6ffdc6de84ad822
zacharydoroshenko <zacharydoroshenko@gmail.com>
2026-02-02 18:57:58 -0800

    serverside

---
+++ zacharydoroshenko.work/public_html/hw2/phpcode/destroy-session-php.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+// 1. Unset all session variables
+$_SESSION = array();
+
+// 2. Destroy the session cookie in the browser
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// 3. Destroy the session on the server
+session_destroy();
+?>
+<!DOCTYPE html>
+<html>
+<head><title>Session Destroyed</title></head>
+<body>
+    <h1>Session Successfully Cleared</h1>
+    <p>The server-side data has been wiped.</p>
+    <a href="/hw2/index-state.html">Back to Start</a>
+</body>
+</html>
